43d451cc9a3d60fe8f70d56011e0aad772687e83
EmielLauwers <lauwers.emiel@gmail.com>
2025-01-23 11:23:56 +0100

    Initial Commit

---
+++ AppNotes/AppNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppNotesApp.swift
+//  AppNotes
+//
+//  Created by Emiel lauwers on 23/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct AppNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppNotes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppNotes
+//
+//  Created by Emiel lauwers on 23/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
