feb73c555ca6231bf05bdc40f3ed4b86c2bd91d3
EmielLauwers <lauwers.emiel@gmail.com>
2025-02-04 14:05:40 +0100

    aanpassingen les dinsdag

---
+++ AppNotes/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  AppNotes
+//
+//  Created by Emiel lauwers on 04/02/2025.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
